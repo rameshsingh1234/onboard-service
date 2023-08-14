3b9d3c6037915f0fccf6ef16a35518b58c84aa06
krishnakr6 <krishna.raj@tibilsolutions.com>
2023-08-14 16:04:00 +0530

    added sonar metafile in .gitignore

---
+++ src/main/python/utils/azure_cred.py
@@ -0,0 +1,6 @@
+# Initialize Azure WAF client
+subscription_id = "389aa082-7b20-41be-b30b-b291a054986e"
+resource_group_name = "sahamati"
+waf_policy_name = "sahamatiWAF"
+azure_application_gateway_name="sahamatiAG"
+
