e517aba7f3b4eb4855846ec0d4a1a4e9ad4ea041
krishnakr6 <krishna.raj@tibilsolutions.com>
2023-08-23 16:16:33 +0530

    Dockerised the app

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,10 @@
-# Docker script to build the image
+FROM python:3.11-slim
+
+WORKDIR /app
+
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+CMD ["gunicorn", "-w", "4", "-b", "0.0.0.0:8000", "src.main.python.main:gunicorn_app"]
--- src/main/python/api/main.py
+++ src/main/python/api/main.py
@@ -1,19 +1,30 @@
 from src.main.python.api.app import app
-from src.main.python.models.database import create_tables,setup_db_config
-from fiu_controller import fiu_blueprint
-from fip_controller import fip_blueprint
-from aa_controller import aa_blueprint
+from src.main.python.models.db_connection import setup_db_config
 
-# Initialize app and set up database configuration
+# app = Flask(__name__)
 setup_db_config(app)
 
-# Create tables before running the app
-create_tables()
 
-# Register the blueprints
-app.register_blueprint(fiu_blueprint, url_prefix='/v1/FIU')
-app.register_blueprint(fip_blueprint, url_prefix='/v1/FIP')
-app.register_blueprint(aa_blueprint, url_prefix='/v1/AA')
+# db = SQLAlchemy(app)
+
+
+def create_app():
+    from src.main.python.models.database import create_tables
+    create_tables()
+
+    from src.main.python.api.fiu_controller import fiu_blueprint
+    from src.main.python.api.fip_controller import fip_blueprint
+    from src.main.python.api.aa_controller import aa_blueprint
+    # Register the blueprints
+    app.register_blueprint(fiu_blueprint, url_prefix='/v1/FIU')
+    app.register_blueprint(fip_blueprint, url_prefix='/v1/FIP')
+    app.register_blueprint(aa_blueprint, url_prefix='/v1/AA')
+
+    return app
+
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    create_app = create_app()
+    create_app.run(port=8000)
+else:
+    gunicorn_app = create_app()
